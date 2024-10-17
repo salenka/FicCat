b1ac222678cea63be442da008a4d2094ef001adf
SandraReginadeAlenca <salenka@gmail.com>
2024-10-17 19:58:03 -0300

    + função uncheckRadio

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // JavaScript source code
 alert("Funcionando");
 
+/*
+Acho que tem um conflito entre o 
+document.getElementById('mais-pessoa').style.display = 'none'; do resp-int-section e o
+document.getElementById('mais-pessoa').style.display = 'block'; do pessoa-section
+porque esse segundo continuará com uma das opões ticada mesmo que ela não esteja visível na página.
+*/
+
 // resp-int-section
 
 document.querySelectorAll('input[name="resp-int"]').forEach(radio => {
@@ -9,30 +16,52 @@ document.querySelectorAll('input[name="resp-int"]').forEach(radio => {
             document.getElementById('pessoa-section').style.display = 'block';
             document.getElementById('entidade-section').style.display = 'none';
             document.getElementById('evento-section').style.display = 'none';
+            
         } else if (document.getElementById('entidade').checked) {
             document.getElementById('pessoa-section').style.display = 'none';
             document.getElementById('entidade-section').style.display = 'block';
             document.getElementById('evento-section').style.display = 'none';
-            document.getElementById('detalhes-pessoa').style.display = 'none';
-            document.getElementById('mais-pessoa').style.display = 'none'; 
-            document.getElementById('qtd-pessoa').style.display = 'none';
+            uncheckRadio('t-pessoa');
+            
+
         } else if (document.getElementById('evento').checked) {
             document.getElementById('pessoa-section').style.display = 'none';
             document.getElementById('entidade-section').style.display = 'none';
             document.getElementById('evento-section').style.display = 'block';
-            document.getElementById('detalhes-pessoa').style.display = 'none';
-            document.getElementById('mais-pessoa').style.display = 'none';
-            document.getElementById('qtd-pessoa').style.display = 'none';
-        }
+            //document.querySelectorAll('input[name="t-pessoa"]:checked') = false;
+            //document.getElementById('pessoa-section').dispatchEvent (new Event('change'));
+        } 
     });
 });
+
+function uncheckRadio(radioName) {
+    const radioTarget = document.querySelector(`input[name="${radioName}"]:checked`); 
+    
+    // desmarca o radio que estiver marcado
+    if (radioTarget) {
+        radioTarget.checked = false;
+    }
+    // dispara o evento 'change' nas opções com radioName
+    const changeEvent = new Event('change');
+    
+    document.querySelectorAll(`input[name="${radioName}`).forEach(radio => {
+        if (radio) {
+        radio.dispatchEvent(changeEvent);
+       } else {
+            console.error(`Elemento ${radioName} não foi encontrado.`);
+              }
+    
+    })
+}
+
+
 
 // pessoa-section
 
 document.querySelectorAll('input[name="t-pessoa"]').forEach(radio => {
     radio.addEventListener('change', function () {
-        document.getElementById('qtd-pessoa').style.display = 'none';//para o caso de mudança de tipo de pessoa
-        if (document.getElementById('autor').checked) {
+        document.getElementById('qtd-pessoa').style.display = 'none'; //para o caso de mudança entre tipos de pessoa
+        if (document.getElementById('autor').checked /*&& document.getElementById('pessoa').checked*/) {
             document.getElementById('autor-section').style.display = 'block';
             document.getElementById('organizador-section').style.display = 'none';
             document.getElementById('coordenador-section').style.display = 'none';
@@ -40,7 +69,7 @@ document.querySelectorAll('input[name="t-pessoa"]').forEach(radio => {
             document.getElementById('editor-section').style.display = 'none';
             document.getElementById('mais-pessoa').style.display = 'block';
 
-        } else if (document.getElementById('organizador').checked) {
+        } else if (document.getElementById('organizador').checked /*&& document.getElementById('pessoa').checked*/)  {
             document.getElementById('autor-section').style.display = 'none';
             document.getElementById('organizador-section').style.display = 'block';//não está mostrando
             document.getElementById('coordenador-section').style.display = 'none';
@@ -48,7 +77,7 @@ document.querySelectorAll('input[name="t-pessoa"]').forEach(radio => {
             document.getElementById('editor-section').style.display = 'none';
             document.getElementById('mais-pessoa').style.display = 'block';
             
-        } else if (document.getElementById('coordenador').checked) {
+        } else if (document.getElementById('coordenador').checked /*&& document.getElementById('pessoa').checked*/) {
             document.getElementById('autor-section').style.display = 'none';
             document.getElementById('organizador-section').style.display = 'none';
             document.getElementById('coordenador-section').style.display = 'block';
@@ -56,7 +85,7 @@ document.querySelectorAll('input[name="t-pessoa"]').forEach(radio => {
             document.getElementById('editor-section').style.display = 'none';
             document.getElementById('mais-pessoa').style.display = 'block';
             
-        } else if (document.getElementById('compilador').checked) {
+        } else if (document.getElementById('compilador').checked /*&& document.getElementById('pessoa').checked*/) {
             document.getElementById('autor-section').style.display = 'none';
             document.getElementById('organizador-section').style.display = 'none';
             document.getElementById('coordenador-section').style.display = 'none';
@@ -64,14 +93,22 @@ document.querySelectorAll('input[name="t-pessoa"]').forEach(radio => {
             document.getElementById('editor-section').style.display = 'none';
             document.getElementById('mais-pessoa').style.display = 'block';
             
-        } else if (document.getElementById('editor').checked) {
+        } else if (document.getElementById('editor').checked /*&& document.getElementById('pessoa').checked*/) {
             document.getElementById('autor-section').style.display = 'none';
             document.getElementById('organizador-section').style.display = 'none';
             document.getElementById('coordenador-section').style.display = 'none';
             document.getElementById('compilador-section').style.display = 'none';
             document.getElementById('editor-section').style.display = 'block';
             document.getElementById('mais-pessoa').style.display = 'block';
-            
+           
+         } else {
+            document.getElementById('autor-section').style.display = 'none';
+            document.getElementById('organizador-section').style.display = 'none';
+            document.getElementById('coordenador-section').style.display = 'none';
+            document.getElementById('compilador-section').style.display = 'none';
+            document.getElementById('editor-section').style.display = 'none';
+            document.getElementById('mais-pessoa').style.display = 'none';
+         
         }
     });
 });
