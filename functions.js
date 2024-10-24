d91ea27f3de8aa7af3365ee79ba758eb375cc84b
SandraReginadeAlenca <salenka@gmail.com>
2024-10-24 18:51:28 -0300

    + CONTRIBUIDORES (Ilustrador e Tradutor) OK! Next..

--- functions.js
+++ functions.js
@@ -27,3 +27,4 @@ export function updateTipoPessoa() {
             span.textContent = pessoaSelecionada;
         });
 }
+
--- script.js
+++ script.js
@@ -100,13 +100,11 @@ document.querySelectorAll('input[name="t-pessoa"]').forEach(radio => {
     document.querySelectorAll('input[name="t-pessoa"]').forEach(radio => {
         radio.addEventListener('change', updateTipoPessoa);
     });
-    
 
 });
 
 
 //MAIS-PESSOA
-
 
 document.querySelectorAll('input[name="mais-pessoa"]').forEach(radio => {
     radio.addEventListener('change', function () {
@@ -145,9 +143,106 @@ document.querySelectorAll('input[name="qtd-pessoa"]').forEach(radio => {
     });
 });
 
-const tipoPessoa = document.querySelector('input[name="t-pessoa"]:checked').value;
+//ILUSTRADOR
 
-document.getElementById('span-tipo-pessoa').textContent = tipoPessoa;
+document.getElementById('ilustrador').addEventListener('change', function () {
+           
+        if (document.getElementById('ilustrador').checked) {
+            document.getElementById('ilustrador-section').style.display = 'block';
+            document.getElementById('mais-ilustrador').style.display = 'block';
 
-alert(tipoPessoa);
+        } else   {
+            document.getElementById('ilustrador-section').style.display = 'none';
+            document.getElementById('mais-ilustrador').style.display = 'none';               
+        } 
+    })
 
+    //MAIS-Ilustrador
+    
+    document.querySelectorAll('input[name="mais-ilustrador"]').forEach(radio => {
+        radio.addEventListener('change', function () {
+            if (document.getElementById('sim-ilustrador').checked) {
+                document.getElementById('qtd-ilustrador').style.display = 'block';
+            } else {
+                document.getElementById('qtd-ilustrador').style.display = 'none';
+            }
+        });
+    });
+    
+    //OUTRO-Ilustrador
+
+    document.querySelectorAll('input[name="qtd-ilustrador"]').forEach(radio => {
+        radio.addEventListener('change', function () {
+    
+    
+            if (document.getElementById('qtd-ilustrador-2').checked) {
+                document.getElementById('segundo-ilustrador').style.display = 'block';
+                document.getElementById('terceiro-ilustrador').style.display = 'none';
+                document.getElementById('quarto-ilustrador').style.display = 'none';
+            } else if (document.getElementById('qtd-ilustrador-3').checked) {
+                document.getElementById('segundo-ilustrador').style.display = 'block';
+                document.getElementById('terceiro-ilustrador').style.display = 'block';
+                document.getElementById('quarto-ilustrador').style.display = 'none';
+            } else if (document.getElementById('qtd-ilustrador-4').checked) {
+                document.getElementById('segundo-ilustrador').style.display = 'none';
+                document.getElementById('terceiro-ilustrador').style.display = 'none';
+                document.getElementById('quarto-ilustrador').style.display = 'block';
+            } else {
+                document.getElementById('segundo-ilustrador').style.display = 'none';
+                document.getElementById('terceiro-ilustrador').style.display = 'none';
+                document.getElementById('quarto-ilustrador').style.display = 'none';
+            }
+        });
+    });
+
+//TRADUTOR
+
+document.getElementById('tradutor').addEventListener('change', function () {
+           
+    if (document.getElementById('tradutor').checked) {
+        document.getElementById('tradutor-section').style.display = 'block';
+        document.getElementById('mais-tradutor').style.display = 'block';
+
+    } else   {
+        document.getElementById('tradutor-section').style.display = 'none';
+        document.getElementById('mais-tradutor').style.display = 'none';              
+    } 
+})
+
+    //MAIS-tradutor
+    
+    document.querySelectorAll('input[name="mais-tradutor"]').forEach(radio => {
+        radio.addEventListener('change', function () {
+            if (document.getElementById('sim-tradutor').checked) {
+                document.getElementById('qtd-tradutor').style.display = 'block';
+            } else {
+                document.getElementById('qtd-tradutor').style.display = 'none';
+            }
+        });
+    });
+
+    //OUTRO-Tradutor
+
+    document.querySelectorAll('input[name="qtd-tradutor"]').forEach(radio => {
+        radio.addEventListener('change', function () {
+    
+    
+            if (document.getElementById('qtd-tradutor-2').checked) {
+                document.getElementById('segundo-tradutor').style.display = 'block';
+                document.getElementById('terceiro-tradutor').style.display = 'none';
+                document.getElementById('quarto-tradutor').style.display = 'none';
+            } else if (document.getElementById('qtd-tradutor-3').checked) {
+                document.getElementById('segundo-tradutor').style.display = 'block';
+                document.getElementById('terceiro-tradutor').style.display = 'block';
+                document.getElementById('quarto-tradutor').style.display = 'none';
+            } else if (document.getElementById('qtd-tradutor-4').checked) {
+                document.getElementById('segundo-tradutor').style.display = 'none';
+                document.getElementById('terceiro-tradutor').style.display = 'none';
+                document.getElementById('quarto-tradutor').style.display = 'block';
+            } else {
+                document.getElementById('segundo-tradutor').style.display = 'none';
+                document.getElementById('terceiro-tradutor').style.display = 'none';
+                document.getElementById('quarto-tradutor').style.display = 'none';
+            }
+        });
+    });
