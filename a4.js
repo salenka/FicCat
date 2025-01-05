b20b738afd9db66d60ca198d5297debb7d88e415
salenka <salenka@gmail.com>
2025-01-05 15:22:10 -0300

    Imagens das licenças em a4.html OK

--- a4.js
+++ a4.js
@@ -1,23 +1,45 @@
-import { geraFicha } from './functions.js';
 
 // Chama a função geraPagina ao carregar a página
 window.onload = function() {
 
     const ficha = JSON.parse(localStorage.getItem('ficha'));
     const codigos = JSON.parse(localStorage.getItem('codigos'));
-        
+    const licenca = localStorage.getItem("licenca");
+    
+
+    switch (licenca) {
+        case "by": 
+            document.getElementById("by").style.display = 'block';
+            break;
+        case "by-sa": 
+            document.getElementById("by-sa").style.display = 'block';
+            break;
+        case "by-nd": 
+            document.getElementById("by-nd").style.display = 'block';
+            break;
+        case "by-nc": 
+            document.getElementById("by-nc").style.display = 'block';
+            break;
+        case "by-nc-sa": 
+            document.getElementById("by-nc-sa").style.display = 'block';
+            break;
+        case "by-nc-nd": 
+            document.getElementById("by-nc-nd").style.display = 'block';
+            break;
+        case "cc-0": 
+            document.getElementById("cc-0").style.display = 'block';
+            break;
+        default:
+            console.log("Licença não selecionada")
+    }
+
+
     document.getElementById("ficha_aqui").textContent = ficha;
     document.getElementById("codigos_aqui").textContent = codigos;
-    /*
-    
-    const iframe = document.getElementById('card-box');
-    const doc = iframe.contentDocument || iframe.contentWindow.document;
 
-    doc.open();
-    doc.write(`<p><pre>${ficha}</pre></p>`);
-    doc.close();
-
-    */
+    console.log(`licença salva no localStorage: ${licenca}`)
         
-    console.log(ficha);
+    console.log(`ficha salva no localStorage: ${codigos}`);
+
+    console.log(`códigos salvos no localStorage: ${ficha}`);
 };
