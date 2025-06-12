8da75bc031166cd90f206cb29a0c040e73cb1d3c
SandraReginadeAlenca <salenka@gmail.com>
2025-06-12 19:20:01 -0300

    Alteração na saída do segundo ISBN

--- cardScript.js
+++ cardScript.js
@@ -527,7 +527,7 @@ qualificador1 = qualificador1 ? ` (${qualificador1})` : "";
 isbn1 = `${isbn1}${qualificador1}`;
 
 let isbn2 = document.getElementById("isbn-2").value.trim();
-isbn2 = isbn2 ? `. — ISBN ${isbn2}.` : "";
+isbn2 = isbn2 ? ` ; ISBN ${isbn2}` : "";
 let qualificador2 = document.getElementById("qualificador-2").value.trim();
 qualificador2 = qualificador2 ? ` (${qualificador2})` : "";
 
